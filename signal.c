32be24805ae462853182171dc81139cf035c69cc
ximena9 <a01742439@tec.mx>
2022-10-06 13:21:08 +0000

    signal.c

--- signal.c
+++ signal.c
@@ -6,13 +6,20 @@ void hola(int signalNum){
     printf("Recibí la señal %d", signalNum);
 }
 
+int cond;
+void terminarWhile(int sigNumber){
+    printf("Terminando while");
+    cond = 0;
+}
+
 int main(){
-    signal(12, hola);
+    signal(12, terminarWhile);
     signal(2, hola);
+    cond = 1;
     while(1){
         printf("trabajando\n");
         sleep(1);
     }
-    printf("Aqui nunca llega");
+    printf(" Aqui nunca llega");
     return 0;
 }
