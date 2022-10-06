e9762d5495cb748a52712a0947cafc0ba82483db
ximena9 <a01742439@tec.mx>
2022-10-06 13:33:44 +0000

    clase

--- tomay.c
+++ tomay.c
@@ -5,11 +5,11 @@ int main(){
     char c;
     char may;
     int n;
-    do(
+    do{
         n= read (STDIN_FILENO, &c, 1);
         may= toupper(c);
         write(STDOUT_FILENO, &may, 1);
     
-    )while(n !=0);
+    }while(n !=0);
     return 0;
 }
